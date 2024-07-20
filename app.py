0fe25f61dab459e2e2881a2d5c960382f0f70809
dennisorion <dennis.orion@hotmail.com>
2024-07-20 22:48:36 +0000

    atual

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from data.cards import all_cards
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    
+    return render_template(
+        'custom_card.html',
+        all_cards = all_cards
+    )
+
+
+if __name__ == '__main__':
+    print(all_cards)
+    app.run(debug=True)
---
+++ cards.py
@@ -0,0 +1,48 @@
+card_data1 = {
+        'title': 'Meu card customizado1',
+        'content': 'Esse é o conteudo customizado do meu card1',
+        'links': [
+            {
+                'href': 'https://youtube.com',
+                'text': 'youtube'
+            },
+            {
+                'href': 'https://google.com',
+                'text': 'google'
+            }
+        ]
+    }
+
+
+card_data2 = {
+        'title': 'Meu card customizado2',
+        'content': 'Esse é o conteudo customizado do meu card2',
+        'links': [
+            {
+                'href': 'https://store.steampowered.com/?l=portuguese',
+                'text': 'steam'
+            },
+            {
+                'href': 'https://www.abola.pt/',
+                'text': 'abola'
+            }
+        ]
+    }
+
+
+card_data3 = {
+        'title': 'Meu card customizado3',
+        'content': 'Esse é o conteudo customizado do meu card3',
+        'links': [
+            {
+                'href': 'https://br.linkedin.com/',
+                'text': 'linkedin'
+            },
+            {
+                'href': 'https://www.workana.com/',
+                'text': 'workana'
+            }
+        ]
+    }
+
+all_cards = [card_data1, card_data2, card_data3]
